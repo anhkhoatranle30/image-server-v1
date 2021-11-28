e4da4152bd2fb1ecaeac2cb196ed2f233c91d1d4
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-28 14:24:10 +0700

    Reduced file size

--- src/api/v2/middlewares/uploadImage.js
+++ src/api/v2/middlewares/uploadImage.js
@@ -2,11 +2,9 @@ import multer from 'multer';
 
 const uploadImage = multer({
   limits: {
-    fileSize: 4 * 1024 * 1024,
+    fileSize: 2 * 1024 * 1024,
   },
   fileFilter(req, file, cb) {
-    console.log('🚀 ~ file: uploadImage.js ~ line 8 ~ fileFilter ~ file', file);
-
     if (!file.originalname.match(/\.(jpg|jpeg|png)/)) {
       return cb(new Error('Only accept pictures'));
     }
